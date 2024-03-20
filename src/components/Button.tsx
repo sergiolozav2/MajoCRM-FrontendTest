90bdd758c408ba02c13f4ae20feab4962a920501
Sergio Loza <sergiolozav@gmail.com>
2024-03-20 15:51:23 -0400

    feat: Menos margen top

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 export function Button(props: ButtonProps) {
   return (
     <button
-      className="mt-8 px-2 py-1 rounded-md bg-black text-stone-50"
+      className="mt-4 px-2 py-1 rounded-md bg-black text-stone-50"
       {...props}
     >
       {props.children}
